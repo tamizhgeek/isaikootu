d63fc7a13e7d1aab2c28766ac0bade08842ae719
Azhagu Selvan SP <tamizhgeek@gmail.com>
2013-08-27 01:40:45 +0530

    Hopefully final setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+
+def readme():
+    with open('README') as f:
+        return f.read()
+
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+
+
 
 setup(
+    cmdclass = {'install' : install},
     name = "isaikootu",
     version = "0.1",
     url = 'http://github.com/tamizhgeek/isaikootu',
     license = 'BSD',
     description = "Indexer and Searcher for multimedia files",
+    long_description = readme(),
     author = 'Azhagu Selvan',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    install_requires = ['setuptools','peewee'],
+    packages = ['isaikootu'],
+    scripts = ['bin/isaikootu'],
+    install_requires = ['setuptools','peewee', 'prettytable', 'eyed3'],
+    zip_safe = False,
+    test_suite='nose.collector',
+    tests_require=['nose'],
 )
